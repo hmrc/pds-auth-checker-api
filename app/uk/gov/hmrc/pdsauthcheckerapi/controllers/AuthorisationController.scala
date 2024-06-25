0aa619b90d65c30e41891fd958909318364b44e8
“WitchellJack” <jack.witchell@digital.hmrc.gov.uk>
2024-06-25 09:49:52 +0100

    UIMS-55 - functionality in place, requires testing

--- app/uk/gov/hmrc/pdsauthcheckerapi/controllers/AuthorisationController.scala
+++ app/uk/gov/hmrc/pdsauthcheckerapi/controllers/AuthorisationController.scala
@@ -16,35 +16,177 @@
 
 package uk.gov.hmrc.pdsauthcheckerapi.controllers
 
-import javax.inject._
+import cats.data.{NonEmptyList, ValidatedNel}
+import cats.syntax.apply._
+import cats.syntax.traverse._
+import cats.syntax.validated._
 import play.api.Configuration
+
+import javax.inject._
 import play.api.libs.json.Json
-import play.api.mvc._
-import uk.gov.hmrc.pdsauthcheckerapi.models
-import uk.gov.hmrc.pdsauthcheckerapi.models.PdsAuthRequest
+import play.api.mvc.{Action, ControllerComponents, Request, Result}
+import uk.gov.hmrc.pdsauthcheckerapi.controllers.AuthorisationController.{
+  validateDate,
+  validateEori
+}
+import uk.gov.hmrc.pdsauthcheckerapi.models.{
+  AuthorisedBadRequestCode,
+  AuthorisedBadRequestResponse,
+  DateValidationError,
+  Eori,
+  EoriValidationError,
+  PdsAuthRequest,
+  UnvalidatedRequest,
+  ValidationError
+}
 import uk.gov.hmrc.pdsauthcheckerapi.services.PdsService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 @Singleton
-class AuthorisationController @Inject()(cc: ControllerComponents, config: Configuration, pdsService: PdsService)(implicit ec: ExecutionContext) extends BackendController(cc)  {
+class AuthorisationController @Inject() (
+    cc: ControllerComponents,
+    config: Configuration,
+    pdsService: PdsService
+)(implicit ec: ExecutionContext)
+    extends BackendController(cc) {
 
-  private val supportedAuthTypes: Set[String] = config.get[String]("auth.supportedTypes").split(",").map(_.trim).toSet
+  def authorise: Action[UnvalidatedRequest] =
+    Action(parse.json[UnvalidatedRequest]).async {
+      implicit request: Request[UnvalidatedRequest] =>
+        if (!supportedAuthTypes.contains(request.body.authType)) {
+          Future.successful(InvalidAuthTypeResponse)
+        } else {
 
-  private val InvalidAuthTypeResponse = BadRequest(Json.obj(
-    "code" -> "INVALID_AUTHTYPE",
-    "message" -> "Auth Type provided is not supported"
-  ))
+          val handleValidationErrors
+              : NonEmptyList[ValidationError] => Future[Result] = errors =>
+            Future.successful(
+              BadRequest(
+                Json.toJson(
+                  AuthorisedBadRequestResponse(
+                    AuthorisedBadRequestCode.InvalidFormat,
+                    "Input format for request data",
+                    errors.toList
+                  )
+                )
+              )
+            )
 
-  def authorise: Action[PdsAuthRequest] = Action(parse.json[models.PdsAuthRequest]).async { implicit request =>
-    val pdsAuthRequestBody = request.body
-    if (!supportedAuthTypes.contains(pdsAuthRequestBody.authType)) {
-      Future.successful(InvalidAuthTypeResponse)
+          val handleValidEorisAndDate
+              : ((Option[LocalDate], Seq[Eori])) => Future[Result] = {
+            case (date, eoris) =>
+              pdsService
+                .getValidatedCustoms(
+                  PdsAuthRequest(date, request.body.authType, eoris)
+                )
+                .map { pdsAuthResponse =>
+                  Ok(Json.toJson(pdsAuthResponse))
+                }
+          }
+
+          val validatedEoris: ValidatedNel[EoriValidationError, Seq[Eori]] = {
+            request.body.eoris.traverse(validateEori)
+          }
+          val validatedDate
+              : ValidatedNel[DateValidationError, Option[LocalDate]] =
+            request.body.validityDate.traverse(validateDate)
+
+          val merged: ValidatedNel[
+            ValidationError,
+            (Option[LocalDate], Seq[Eori])
+          ] =
+            (
+              validatedDate,
+              validatedEoris
+            ).tupled
+          merged.fold(
+            handleValidationErrors,
+            handleValidEorisAndDate
+          )
+        }
+    }
+
+  private val InvalidAuthTypeResponse = BadRequest(
+    Json.obj(
+      "code" -> "INVALID_AUTHTYPE",
+      "message" -> "Auth Type provided is not supported"
+    )
+  )
+
+  private val supportedAuthTypes: Set[String] =
+    config.get[String]("auth.supportedTypes").split(",").map(_.trim).toSet
+}
+
+object AuthorisationController {
+  private def validateDate(
+      rawDate: String
+  ): ValidatedNel[DateValidationError, LocalDate] =
+    Try(LocalDate.parse(rawDate, DateTimeFormatter.ISO_LOCAL_DATE)).fold(
+      _ =>
+        DateValidationError(
+          rawDate,
+          "Invalid Format: Dates must use ISO-8601 format YYYY-MM-DD"
+        ).invalidNel,
+      _.valid
+    )
+
+  private def validateEori(
+      rawEori: String
+  ): ValidatedNel[EoriValidationError, Eori] =
+    (validateCountryCode(rawEori), validateEoriDigits(rawEori)).mapN((_, _) =>
+      Eori(rawEori)
+    )
+
+  private def validateCountryCode(
+      rawEori: String
+  ): ValidatedNel[EoriValidationError, String] = {
+    val allegedCountryCode = rawEori.takeWhile(!_.isDigit)
+    if (validCountryCodes.contains(allegedCountryCode)) {
+      rawEori.validNel
     } else {
-      pdsService.getValidatedCustoms(pdsAuthRequestBody).map { pdsAuthResponse =>
-        Ok(Json.toJson(pdsAuthResponse))
-      }
+      EoriValidationError(
+        rawEori,
+        invalidCountryCodeMessage(allegedCountryCode)
+      ).invalidNel
     }
   }
+
+  private val validCountryCodes = Seq("GB", "XI")
+
+  private def invalidCountryCodeMessage(invalidCode: String): String =
+    s"Invalid Format: $invalidCode is not a supported country code"
+
+  private def validateEoriDigits(
+      rawEori: String
+  ): ValidatedNel[EoriValidationError, String] = {
+    val allegedDigits = rawEori.dropWhile(!_.isDigit)
+
+    val errors = List(
+      if (allegedDigits.exists(!_.isDigit))
+        Some(
+          "Invalid Format: EORI must start with GB or XI and be followed by 12 digits"
+        )
+      else None,
+      if (allegedDigits.length < 12)
+        Some("Invalid Format: Too few digits")
+      else None,
+      if (allegedDigits.length > 12)
+        Some("Invalid Format: Too many digits")
+      else None
+    ).flatten
+
+    if (errors.nonEmpty) {
+      NonEmptyList
+        .fromList(errors.map(err => EoriValidationError(rawEori, err)))
+        .get
+        .invalid
+    } else {
+      rawEori.validNel
+    }
+  }
+
 }
