39c3c6bdb677f149a89f05309c9de8aef4013a1b
RobertLaverick <10117445+RobertLaverick@users.noreply.github.com>
2024-08-13 16:30:18 +0100

    updated dependencies to latest versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,20 +5,19 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.5.0"
-  
+  private val bootstrapVersion = "9.3.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "io.lemonlabs"            %% "scala-uri"                  % "3.6.0"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "io.lemonlabs" %% "scala-uri" % "3.6.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
-    "org.scalamock"           %% "scalamock"                  % "6.0.0",
-    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.18.0",
-    "org.scalatestplus"       %% "mockito-3-4"                % "3.2.10.0"
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion,
+    "org.scalamock" %% "scalamock" % "6.0.0",
+    "org.scalatestplus" %% "scalacheck-1-17" % "3.2.18.0",
+    "org.scalatestplus" %% "mockito-3-4" % "3.2.10.0"
   ).map(_ % Test)
 
-  val it = Seq.empty
+  val it = test
 }
